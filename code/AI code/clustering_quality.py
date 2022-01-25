5d826b90405ec417cb9a3f5e67a0534c89f4f57e
TarlokYT <98346220+TarlokYT@users.noreply.github.com>
2022-01-25 16:28:42 -0500

    Create clustering_quality.py

---
+++ code/AI code/clustering_quality.py
@@ -0,0 +1,39 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import metrics
+from sklearn.cluster import KMeans
+
+X = np.loadtxt('data_quality.txt', delimiter=',')
+
+plt.figure()
+plt.scatter(X[:,0], X[:,1], color='black', s=80, marker='o', facecolors='none')
+x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+plt.title('Input data')
+plt.xlim(x_min, x_max)
+plt.ylim(y_min, y_max)
+plt.xticks(())
+plt.yticks(())
+
+scores = []
+values = np.arange(2, 10)
+
+for num_clusters in values:
+    kmeans = KMeans(init='k-means++', n_clusters=num_clusters, n_init=10)
+    kmeans.fit(X)
+    score = metrics.silhouette_score(X, kmeans.labels_, 
+                metric='euclidean', sample_size=len(X))
+
+    print("\nNumber of clusters =", num_clusters)
+    print("Silhouette score =", score)
+                    
+    scores.append(score)
+
+plt.figure()
+plt.bar(values, scores, width=0.7, color='black', align='center')
+plt.title('Silhouette score vs number of clusters')
+
+num_clusters = np.argmax(scores) + values[0]
+print('\nOptimal number of clusters =', num_clusters)
+
+plt.show()
