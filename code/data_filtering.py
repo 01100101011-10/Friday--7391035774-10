7c5bff93fdff2ff9341c3a5deb652be1d2a78d06
TarlokYT <98346220+TarlokYT@users.noreply.github.com>
2022-01-25 15:11:36 -0500

    Update data_filtering.py

--- code/data_filtering.py
+++ code/data_filtering.py
@@ -10,20 +10,15 @@ def build_arg_parser():
             help='Input user')
     return parser
 
-# Finds users in the dataset that are similar to the input user 
 def find_similar_users(dataset, user, num_users):
     if user not in dataset:
         raise TypeError('Cannot find ' + user + ' in the dataset')
 
-    # Compute Pearson score between input user 
-    # and all the users in the dataset
     scores = np.array([[x, pearson_score(dataset, user, 
             x)] for x in dataset if x != user])
 
-    # Sort the scores in decreasing order
     scores_sorted = np.argsort(scores[:, 1])[::-1]
 
-    # Extract the top 'num_users' scores
     top_users = scores_sorted[:num_users] 
 
     return scores[top_users] 
