b78c965bb8b69879f0a182b6d788b11761f8b937
TarlokYT <98346220+TarlokYT@users.noreply.github.com>
2022-01-25 11:24:20 -0500

    Update visualization.py

--- code/visualization.py
+++ code/visualization.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 from deap import algorithms, base, benchmarks, \
         cma, creator, tools
 
-# Function to create a toolbox
 def create_toolbox(strategy):
     creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
     creator.create("Individual", list, fitness=creator.FitnessMin)
@@ -11,7 +10,6 @@ def create_toolbox(strategy):
     toolbox = base.Toolbox()
     toolbox.register("evaluate", benchmarks.rastrigin)
 
-    # Seeed the random number generator
     np.random.seed(7)
 
     toolbox.register("generate", strategy.generate, creator.Individual)
@@ -20,21 +18,16 @@ def create_toolbox(strategy):
     return toolbox
 
 if __name__ == "__main__":
-    # Problem size
     num_individuals = 10
     num_generations = 125
 
-    # Create a strategy using CMA-ES algorithm
     strategy = cma.Strategy(centroid=[5.0]*num_individuals, sigma=5.0, 
             lambda_=20*num_individuals)
 
-    # Create toolbox based on the above strategy
     toolbox = create_toolbox(strategy)
 
-    # Create hall of fame object
     hall_of_fame = tools.HallOfFame(1)
 
-    # Register the relevant stats
     stats = tools.Statistics(lambda x: x.fitness.values)
     stats.register("avg", np.mean)
     stats.register("std", np.std)
@@ -44,7 +37,6 @@ if __name__ == "__main__":
     logbook = tools.Logbook()
     logbook.header = "gen", "evals", "std", "min", "avg", "max"
     
-    # Objects that will compile the data
     sigma = np.ndarray((num_generations, 1))
     axis_ratio = np.ndarray((num_generations, 1))
     diagD = np.ndarray((num_generations, num_individuals))
@@ -53,26 +45,20 @@ if __name__ == "__main__":
     std = np.ndarray((num_generations, num_individuals))
 
     for gen in range(num_generations):
-        # Generate a new population
         population = toolbox.generate()
 
-        # Evaluate the individuals
         fitnesses = toolbox.map(toolbox.evaluate, population)
         for ind, fit in zip(population, fitnesses):
             ind.fitness.values = fit
         
-        # Update the strategy with the evaluated individuals
         toolbox.update(population)
         
-        # Update the hall of fame and the statistics with the
-        # currently evaluated population
         hall_of_fame.update(population)
         record = stats.compile(population)
         logbook.record(evals=len(population), gen=gen, **record)
         
         print(logbook.stream)
         
-        # Save more data along the evolution 
         sigma[gen] = strategy.sigma
         axis_ratio[gen] = max(strategy.diagD)**2/min(strategy.diagD)**2
         diagD[gen, :num_individuals] = strategy.diagD**2
@@ -80,7 +66,6 @@ if __name__ == "__main__":
         best[gen, :num_individuals] = hall_of_fame[0]
         std[gen, :num_individuals] = np.std(population, axis=0)
 
-    # The x-axis will be the number of evaluations
     x = list(range(0, strategy.lambda_ * num_generations, strategy.lambda_))
     avg, max_, min_ = logbook.select("avg", "max", "min")
     plt.figure()
