d04a8b813c01742f8168307afb772c4dd342d9ba
TarlokYT <98346220+TarlokYT@users.noreply.github.com>
2022-01-25 15:25:06 -0500

    Update recurrent_neural_network.py

--- code/neuralsystem/recurrent_neural_network.py
+++ code/neuralsystem/recurrent_neural_network.py
@@ -3,13 +3,11 @@ import matplotlib.pyplot as plt
 import neurolab as nl
 
 def get_data(num_points):
-    # Create sine waveforms
     wave_1 = 0.5 * np.sin(np.arange(0, num_points))
     wave_2 = 3.6 * np.sin(np.arange(0, num_points))
     wave_3 = 1.1 * np.sin(np.arange(0, num_points))
     wave_4 = 4.7 * np.sin(np.arange(0, num_points))
 
-    # Create varying amplitudes
     amp_1 = np.ones(num_points)
     amp_2 = 2.1 + np.zeros(num_points) 
     amp_3 = 3.2 * np.ones(num_points) 
@@ -20,7 +18,6 @@ def get_data(num_points):
 
     return wave, amp 
 
-# Visualize the output 
 def visualize_output(nn, num_points_test):
     wave, amp = get_data(num_points_test)
     output = nn.sim(wave)
@@ -28,25 +25,19 @@ def visualize_output(nn, num_points_test):
     plt.plot(output.reshape(num_points_test * 4))
 
 if __name__=='__main__':
-    # Create some sample data
     num_points = 40
     wave, amp = get_data(num_points)
 
-    # Create a recurrent neural network with 2 layers
     nn = nl.net.newelm([[-2, 2]], [10, 1], [nl.trans.TanSig(), nl.trans.PureLin()])
 
-    # Set the init functions for each layer 
     nn.layers[0].initf = nl.init.InitRand([-0.1, 0.1], 'wb')
     nn.layers[1].initf = nl.init.InitRand([-0.1, 0.1], 'wb')
     nn.init()
 
-    # Train the recurrent neural network
     error_progress = nn.train(wave, amp, epochs=1200, show=100, goal=0.01)
 
-    # Run the training data through the network
     output = nn.sim(wave)
 
-    # Plot the results
     plt.subplot(211)
     plt.plot(error_progress)
     plt.xlabel('Number of epochs')
@@ -57,7 +48,6 @@ if __name__=='__main__':
     plt.plot(output.reshape(num_points * 4))
     plt.legend(['Original', 'Predicted'])
 
-    # Testing the network performance on unknown data 
     plt.figure()
 
     plt.subplot(211)
