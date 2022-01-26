ae4aa89dd668d831bc82d5a289c2a59815285691
TarlokYT <98346220+TarlokYT@users.noreply.github.com>
2022-01-26 12:50:57 -0500

    Create balancer.py

---
+++ code/AI code/balancer.py
@@ -0,0 +1,36 @@
+import argparse
+
+import gym
+
+def build_arg_parser():
+    parser = argparse.ArgumentParser(description='Run an environment')
+    parser.add_argument('--input-env', dest='input_env', required=True,
+            choices=['cartpole', 'mountaincar', 'pendulum'], 
+            help='Specify the name of the environment')
+    return parser
+
+if __name__=='__main__':
+    args = build_arg_parser().parse_args()
+    input_env = args.input_env
+
+    name_map = {'cartpole': 'CartPole-v0', 
+                'mountaincar': 'MountainCar-v0',
+                'pendulum': 'Pendulum-v0'}
+
+    env = gym.make(name_map[input_env])
+
+    for _ in range(20):
+        observation = env.reset()
+
+        for i in range(100):
+            env.render()
+
+            print(observation)
+
+            action = env.action_space.sample()
+
+            observation, reward, done, info = env.step(action)
+            
+            if done:
+                print('Episode finished after {} timesteps'.format(i+1))
+                break
